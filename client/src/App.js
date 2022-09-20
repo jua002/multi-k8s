a382b0e252d22a0433e2d385e71a40bdded7455c
Joe <joe.attah1@icloud.com>
2022-09-20 15:13:47 -0500

    change greeting message

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learning React is Fun
+            Learning React is Great
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
